bac21e8a4d5ea6ca26699056dc14a5bd83fc914b
MTKnight <god18009@byui.edu>
2020-03-09 21:31:57 -0700

    http

---
+++ W05 HTTP/src/http_package/MTKnightHTTP.java
@@ -0,0 +1,37 @@
+/**
+ * @author Marci Godfrey
+ * CIT 360: HTTP
+ */
+
+package http_package;
+import java.io.*;
+import java.net.*;
+
+public class MTKnightHTTP {
+
+	    public static void main(String[] args) throws IOException {
+	    	
+            String inputLine = null;
+            
+	        try {
+	            URL url = new URL("https://www.churchofjesuschrist.org");
+	            HttpURLConnection m = (HttpURLConnection) url.openConnection();
+	            BufferedReader k = new BufferedReader(new InputStreamReader(m.getInputStream()));
+	            
+	            do {
+	            	System.out.println(inputLine);
+	            }
+	            	
+	            while ((inputLine = k.readLine()) != null);
+	            
+	            k.close();
+	            m.disconnect();
+	        } 
+	        
+	        	catch (Exception e) {
+	        		System.out.println(e);
+	        }
+	        
+	    }
+	    
+}
